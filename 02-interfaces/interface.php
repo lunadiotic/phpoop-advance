c3a42491c7ec4cd8249aaf3dc502fc49148d6d30
IDStack <idstack@outlook.co.id>
2020-05-05 16:35:18 +0700

    extend interface

--- 02-interfaces/interface.php
+++ 02-interfaces/interface.php
@@ -5,13 +5,24 @@ interface TableInterface
     public function save(array $data);
 }
 
-class Table implements TableInterface
+interface LogInterface
+{
+    public function log(string $message);
+}
+
+class Table implements TableInterface, LogInterface
 {
     public function save(array $data)
     {
         return "Save method from table class";
     }
+
+    public function log(string $message)
+    {
+        return $message . "\n";
+    }
 }
 
 
+echo (new Table)->log('Interface sample from Table class.');
 echo (new Table)->save([]);
